3b872fd7cad5b8a262968fd137764a547cecee47
Maria Clara Fernandes <fernandes.mcfo@gmail.com>
2022-12-17 19:25:44 -0300

    Ajustes
    Co-authored-by: Pedro Rian <Pleonasmo@users.noreply.github.com>

--- main.c
+++ main.c
@@ -1,48 +1,10 @@
-/********************************************************************
-  Bot-exemplo do jogo Uno
-
-  Esta é uma versão do jogo de baralho Uno, com algumas diferenças:
-
-  A primeira é que este é um jogo baseado em turnos. Ou seja, um jogador só age quando
-  for sua vez. Logo, não haverá a opção de gritar "UNO" quando um jogador tiver apenas
-  uma carta. A regra de dizer UNO foi deixada de fora. Porém, para descontrair e liberar
-  todo o nervosismo da partida, seu bot pode falar o que quiser na sua vez (com seu
-  devido respeito!).
-
-  A segunda é que ele é jogado com as cartas do baralho tradicional. Ou seja, ao invés de
-  cores, teremos naipes (copas, espadas, ouro e paus) e, ao invés de cartas especiais (como
-  "Compre duas" ou "Passe a vez"), teremos cartas do baralho como Valete, Damas, Rei, Ás
-  e Coringa. Estas cartas substituem as cartas especiais do Uno, seguindo a correspondência:
+/*
     "COMPRE 4"   : C = CORINGA
     "COMPRE 2"   : V = VALETE
     "VOLTA"      : D = DAMA
     "PULA A VEZ" : R = REI
     "MUDE A COR" : A = ÀS (muda o naipe)
-
-  A partida será jogada com um único baralho. Assim, teremos quatro cartas de um valor. Por
-  exemplo, 7♥, 7♦, 7♣, 7♠, com exceção do coringa, que há apenas duas: um vermelho e um preto.
-  Porém, para seguir o padrão das cartas, os coringas terão também um naipe, mas serão apenas
-  dos naipes: ♥ (coringa vermelho) e ♣ (coringa preto).
-
-  O jogo é gerenciado por um "simulador" que irá controlar o jogo, dando a vez ao bot da
-  rodada. O bot deve ler os dados da entrada-padrão (via scanf) no formato especificado pelo
-  simulador, caso contrário ele se perderá e será eliminado da partida.
-  O bot também deve enviar suas ações para o simulador via saída-padrão (via printf) no formato
-  esperado pelo simulador, novamente sob pena de ser eliminado da partida.
-
-  As cartas do baralho sempre são no formato "ValorNaipe".
-  Nesse formato, Valor é um dos valores das cartas tradicionais, ou seja, A, 2, 3, 4, 5, 6,
-  7, 8, 9, 10, V (valete), D (dama), R (rei) e C (coringa), e Naipe é um dos seguintes naipes:
-  ♥, ♦, ♣, ♠.
-
-  Os naipes são caracteres no formato ascii estendidos, que possuem uma representação maior
-  que um byte (char). Isso significa que eles deve ser tratados como se fossem strings.
-
-  A lógica apresentada nesse template visa ilustrar a entrada e saída de dados de um bot.
-  Cabe a você aprimorar a lógica das ações do seu bot.
-
-  Bom jogo!!!
- ********************************************************************/
+*/
 
 #include <stdio.h>
 #include <string.h>
@@ -56,57 +18,49 @@
 #define false 0
 #define true 1
 
-char tiposDeCartas = {'A', '2', '3', '4', ' 5', '6',
-                      '7', '8', '9', '10', 'V', 'D', 'R', 'C'};
 char tiposDeNaipes[4][4] = {{"♥"},
                             {"♦"},
                             {"♣"},
                             {"♠"}};
 
-/**
- * Para "debugar", é possível enviar uma mensagem para a saída de erro padrão (stderr).
- * Essa mensagem não será enviada para o simulador, apenas para o terminal.
- */
 void debug(char *message)
 {
   fprintf(stderr, "%s\n", message);
 }
 
-FILE *addCardFile(char vetor[], char caracter[], char endereco[MAX_LINE], char tipoAbertura[], char caracterRemove[])
+FILE *addCardFile(char vetor[], char endereco[MAX_LINE], char tipoAbertura[], char caracterRemove[])
 {
+ // printf("entrou em addCardFile\n");
+  //printf("%s\n", vetor);
   FILE *file;
   char *variavel;
+  //printf("Antes de abrir arquivo");
   file = fopen(endereco, tipoAbertura);
+  //printf("abriu o arquivo\n");
   if (file == NULL)
   {
-    printf("Problemas ao abrir o arquivo");
+    //printf("Problemas ao abrir o arquivo\n");
   }
-  if (caracter != NULL)
-  {
-    variavel = strtok(vetor, caracter);
-    fprintf(file, "%s\n", variavel);
 
-    while (variavel != NULL)
-    {
-      variavel = strtok(NULL, caracter);
-      if (caracterRemove != NULL)
-      {
-        if (strcmp(variavel, caracterRemove) == 0)
-        {
-          break;
-        }
-      }
-      fprintf(file, "%s\n", variavel);
-    }
+  variavel = strtok(vetor, " ");
+
+  while (variavel != NULL && variavel[0] != ']')
+  {
+    fprintf(file, "%s\n", variavel);
+    //printf("Entrou no while\n");
+    variavel = strtok(NULL, " ");
+
+    // printf("Entrou no if caracterRemove\n");
   }
-  fprintf(file, "%s\n", vetor);
+ //printf("Saiu do while\n");
+
   fclose(file);
   return file;
 }
 
 FILE *removerInfo(char info[], char endereco[], int quantCartas)
 {
-
+  //printf("entrou em removerInfo\n");
   char vetorCartas[quantCartas][8];
   FILE *file;
   char carta[8];
@@ -115,7 +69,7 @@ FILE *removerInfo(char info[], char endereco[], int quantCartas)
 
   for (int i = 0; i <= quantCartas; i++)
   {
-    fscanf(file, "%[^\n]\n", carta);
+    fscanf(file, "%s\n", carta);
     if (strcmp(info, carta) != 0)
     {
       strcpy(vetorCartas[contador], carta);
@@ -128,6 +82,7 @@ FILE *removerInfo(char info[], char endereco[], int quantCartas)
   for (int i = 0; i < quantCartas; i++)
   {
     fprintf(file, "%s\n", vetorCartas[i]);
+      //printf("Adicionou: %s\n", vetorCartas[i]);
   }
   fclose(file);
   return file;
@@ -135,7 +90,7 @@ FILE *removerInfo(char info[], char endereco[], int quantCartas)
 
 int verificarSeEhDez(char carta[])
 {
-
+  //printf("entrou em verificarSeEhDez\n");
   if (carta[1] == 0 && carta[0] == 1)
   {
     return true;
@@ -144,10 +99,12 @@ int verificarSeEhDez(char carta[])
   {
     return false;
   }
+  //printf("saiu de verificarSeEhDez");
 }
 
 void mudarNaipe(char carta[], char *naipeDaVez)
 {
+  // printf("Entrou no mudar naipe\n");
   char naipe[4];
   int ehDez = false;
   ehDez = verificarSeEhDez(carta);
@@ -166,16 +123,15 @@ void mudarNaipe(char carta[], char *naipeDaVez)
     contador++;
   }
   strcpy(naipeDaVez, naipe);
+  //printf("Sainda naipe função: %s\n", naipeDaVez);
 }
 
 int main()
 {
-  // Obs: As variáveis deste template foram definidas apenas para o código compilar e rodar.
-  // Então, cabe a você usar as variáveis adequadas em função do que está lendo.
   FILE *arquivoCartas, *arquivoCartasPlayers;
 
-  char hand[MAX_LINE];     // string para leitura temporária de dados
-  char my_id[MAX_ID_SIZE]; // identificador do seu bot
+  char hand[MAX_LINE];
+  char my_id[MAX_ID_SIZE];
   char cardTable[MAX_ACTION];
   char players[MAX_LINE];
 
@@ -187,8 +143,8 @@ int main()
   int outroJogadorComprou = false;
   char endArqCartas[] = "Arquivos/cartas.txt";
 
-  setbuf(stdin, NULL);  // stdin, stdout e stderr não terão buffers
-  setbuf(stdout, NULL); // assim, nada é "guardado temporariamente"
+  setbuf(stdin, NULL);
+  setbuf(stdout, NULL);
   setbuf(stderr, NULL);
 
   scanf("PLAYERS %[^\n]\n", players);
@@ -196,7 +152,7 @@ int main()
   scanf("HAND [ %[^\n]\n", hand);
   scanf("TABLE %s\n", cardTable);
 
-  arquivoCartas = addCardFile(hand, " \0", "Arquivos/cartas.txt", "w\0", "]\0");
+  arquivoCartas = addCardFile(hand, "Arquivos/cartas.txt", "w\0", "]\0");
 
   // === PARTIDA ===
 
@@ -214,26 +170,30 @@ int main()
 
       if (strcmp(action, "DISCARD") == 0)
       {
-        printf("Entroy no if do discard\n");
+        //printf("Entroy no if do discard\n");
 
         if ((complement[0] == 'A') || (complement[0] == 'C'))
         {
 
           scanf("%s", naipeDaVez);
-          printf("Naipe: %s\n", naipeDaVez);
+          //   printf("Naipe: %s\n", naipeDaVez);
         }
         if ((complement[0] != 'A') && (complement[0] != 'C'))
         {
-          printf("Entrou no segundo if\n");
+          // printf("Entrou no segundo if\n");
           mudarNaipe(complement, &naipeDaVez);
 
-          // printf("Naipe diferente:  %s\n", naipeDaVez);
+          //        printf("Naipe diferente:  %s\n", naipeDaVez);
         }
       }
-      if ((strcmp(action, "TURN") != 0) && (strcmp(action, "DISCARD") != 0))
+      else
       {
-        printf("Entrou no else\n");
-        mudarNaipe(cardTable, &naipeDaVez);
+
+        if (strcmp(action, "TURN") != 0)
+        {
+          // printf("Entrou no else\n");
+          mudarNaipe(cardTable, &naipeDaVez);
+        }
       }
       getchar();
 
@@ -241,38 +201,6 @@ int main()
       {
         strcpy(cardTable, complement);
       }
-
-      /*
-      Enquanto não chega a vez do seu bot, ele estará "escutando" todos os eventos
-      do jogo. Estes eventos são repassados para todos os bots em uma linha no formato:
-        <ação> <complemento1> [complemento2]
-
-      Ou seja, <ação> <complemento1> estão sempre presentes na mensagem do evento, porém
-      a presença de [complemento2] vai depender da ação e do complemento1.
-      Por exemplo, se um bot descartar um 7 de paus, será gerado o seguinte evento:
-        DISCARD 7♣
-      A ação é DISCARD e o complemento é 7♣. Portanto, o próximo bot deverá descartar ou
-      um 7 (de qualquer naipe) ou uma carta do naipe ♣. Guarde essa informação porque o
-      próximo bot poderá ser o seu.
-
-      Se a carta descartada for, por exemplo, A♣ (Ás = muda de cor), haverá um segundo
-      complemento com o naipe a ser seguido pelos próximos jogadores. Por exemplo: no
-      evento "DISCARD A♣ ♥", o próximo bot deverá então descartar alguma carta do naipe ♥.
-
-      O valor da carta descartada pode também pedir uma ação do próximo jogador. Por
-      exemplo, se for descartado o V (valete = compre 2), a primeira ação do próximo
-      bot (pode ser o seu) deverá ser obrigatoriamente "BUY 2", sob pena do bot ser
-      eliminado da partida.
-      */
-
-      // obs: um segundo scanf pode ser realizado par ler o 2º complemento.
-
-      /*
-      Há um evento especial que não é gerado pelos outros bots, mas pelo simulador.
-      Ele tem o formato: "TURN <id>".
-      O simulador envia esta mensagem quando for a vez do bot de identificador <id>.
-      Então, termine este laço interno quando for a vez do seu bot agir.
-      */
     } while (strcmp(action, "TURN") || strcmp(complement, my_id));
 
     // agora é a vez do seu bot jogar
@@ -296,43 +224,42 @@ int main()
       for (int i = 0; i < quant; i++)
       {
         scanf("%s\n", hand);
+        arquivoCartas = addCardFile(hand, endArqCartas, "a\0", NULL);
       }
-
-      arquivoCartas = addCardFile(hand, NULL, endArqCartas, "a\0", NULL);
     }
     else
     {
 
-      //  printf("Entrou no else\n");
+      //printf("Entrou no else\n");
 
       arquivoCartas = fopen(endArqCartas, "r");
       if (arquivoCartas == NULL)
       {
         printf("Error ao abrir o arquivo\n");
       }
-      // printf("Abriu o arquivo\n");
+      //printf("Abriu o arquivo\n");
       ehDez = verificarSeEhDez(cardTable);
-      // printf("Passou da função ehDez\n");
-      // printf("É dez: %d\n", ehDez);
+      //printf("Passou da função ehDez\n");
+      //printf("É dez: %d\n", ehDez);
       int j = 0;
-      //  printf("Quant Cartas: %d\n", quantCartas);
+      //printf("Quant Cartas: %d\n", quantCartas);
       while (j < quantCartas)
       {
-        // printf("Entrou no while\n");
+        //printf("Entrou no while\n");
         int valor = 5, i = 1;
 
         int testador = 0;
         char cartaTeste[6];
-        // printf("Passa aqui\n");
+        //printf("Passa aqui\n");
 
         fscanf(arquivoCartas, "%s\n", cartaTeste);
-        // printf("Depois do fscanf\n");
+        //printf("Depois do fscanf\n");
         if (ehDez == true)
         {
 
-          // printf("É dez: %d\n", ehDez);
+          //printf("É dez: %d\n", ehDez);
           ehDez = verificarSeEhDez(cartaTeste);
-          // printf("É dez depois da função: %d\n", ehDez);
+          //printf("É dez depois da função: %d\n", ehDez);
           if (ehDez == true)
           {
             valor = 6;
@@ -340,37 +267,44 @@ int main()
           }
         }
 
-        // printf("Carta teste: %c || cardTable: %c\n", cartaTeste[0], cardTable[0]);
-        if ((cartaTeste[0] == cardTable[0]) || (ehDez == true))
+        //printf("Carta teste: %c || cardTable: %c\n", cartaTeste[0], cardTable[0]);
+        if (((cartaTeste[0] == cardTable[0])) || (ehDez == true))
         {
 
-          // printf("Entrou na fução if\n");
-          printf("DISCARD %s", cartaTeste);
+          //printf("Entrou na fução if\n");
 
           debug("Passou do printf");
-          if ((cartaTeste[0] == 'A') || (cartaTeste[0] == 'C'))
+          if (cartaTeste[0] == 'A')
           {
             debug("Entrou aqui");
-            printf(" %s", tiposDeNaipes[3]);
-           
+            char simbolo[] = "♥";
+            printf("DISCARD %s %s\n", cartaTeste, simbolo);
           }
-          printf("\n");
-          // printf("Cardtable antes da função: %s\n", cardTable);
+          if (cartaTeste[0] == 'C')
+          {
+            printf("DISCARD %s %s\n", cartaTeste, "♥");
+          }
+          else
+          {
+            printf("DISCARD %s\n", cartaTeste);
+          }
+
+          //printf("Cardtable antes da função: %s\n", cardTable);
           strcpy(cardTable, cartaTeste);
-          // printf("Cardtable depois da função: %s\n", cardTable);
+          //printf("Cardtable depois da função: %s\n", cardTable);
           quantCartas--;
           removerInfo(cartaTeste, endArqCartas, quantCartas);
+
           break;
         }
-        // printf("Carta teste: %s || Naipe da vez: %s\n", cartaTeste, naipeDaVez);
+        //printf("Carta teste: %s || Naipe da vez: %s\n", cartaTeste, naipeDaVez);
         for (i; i < valor; i++)
         {
-
           if (cartaTeste[i] == naipeDaVez[testador])
           {
             testador++;
           }
-          // printf("Testador: %d\n", testador);
+          //printf("Testador: %d\n", testador);
         }
 
         if (testador == 4)
@@ -382,7 +316,7 @@ int main()
           break;
         }
         j++;
-        // printf("Valor j: %d\n", j);
+         //printf("Valor j: %d\n", j);
       }
 
       fclose(arquivoCartas);
@@ -390,101 +324,12 @@ int main()
       {
         printf("BUY 1\n");
         scanf("%s\n", hand);
-        arquivoCartas = addCardFile(hand, NULL, "Arquivos/cartas.txt", "a\0", NULL);
+       //printf("Hand: %s\n", hand);
+        arquivoCartas = addCardFile(hand, endArqCartas, "a\0", "]\0");
         quantCartas++;
-        // printf("Passou no ultimo if %d\n", &quantCartas);
+        //printf("Passou no ultimo if %d\n", quantCartas);
       }
     }
-
-    /*
-    Seu bot realiza uma ação no jogo enviando para a saída-padrão uma string no formato:
-      <ação> <complemento1> [complemento2]
-
-    Por exemplo, se o bot anterior soltar uma carta de compra (compre 2 ou compre 4), a
-    <ação> que seu bot deve fazer é "BUY" e o <complemento1> será "2" ou "4", dependendo da
-    quantidade de cartas que ele deve comprar.
-    Ou seja, a string que seu bot deve enviar para a saída-padrão será:
-    - "BUY 4", se o bot anterior soltou um Coringa (C), ou;
-    - "BUY 2", se o bot anterior soltou um Valete (V).
-    Depois do envio desta ação, o simulador irá enviar para o seu bot uma quantidade de cartas
-    correspondente ao número solicitado. Então, prepare-se para ler da entrada padrão as cartas.
-    Se a ação for "BUY 2", leia duas strings. Elas serão as cartas compradas e você deve
-    guardá-las na sua mão. Se for "BUY 4", leia 4 strings.
-    Depois da leitura, termina a vez do seu bot e o simulador passa a vez para um outro bot.
-
-    Caso não tenha nenhuma ação de compra a ser realizada, seu bot deve jogar normalmente,
-    que é descartar uma carta cujo valor ou naipe é o mesmo da carta da mesa. Ou seja, você
-    deve saber qual a última carta descartada ou, se foi um Ás ou Coringa, qual o naipe
-    solicitado.
-
-    No exemplo abaixo, a <ação> é "DISCARD" e o <complemento1> é a carta a ser descartada:
-      "DISCARD 2♣"
-
-    O bot também pode descartar uma carta especial, independente do naipe da mesa, que pode
-    ser um Ás (A = muda de naipe) ou um Coringa (C = além de fazer o próximo comprar 4,
-    também muda o naipe). As demais cartas devem obrigatoriamente seguir o naipe da mesa.
-
-    Ao descartar um Ás ou Coringa, você deve enviar um segundo complemento para sua ação com
-    o naipe que você deseja. Por exemplo:
-      "DISCARD C♣ ♥" 1
-    Neste caso, seu bot soltou um coringa preto e pediu para o naipe mudar para ♥ (o próximo
-    jogador precisar comprar 4 cartas e o seguinte levar em conta que o ♥ é o naipe da vez).
-    Depois do descarte, a vez do seu bot termina.
-
-    Se o bot não tiver carta com o naipe da mesa para descartar, ele deve comprar uma carta
-    do baralho, enviando a ação "BUY" e o complemento "1", informando que ele irá comprar uma
-    carta da pilha.
-    Assim como as ações "BUY 2" e "BUY 4", após o envio desta ação, seu bot deve ler da
-    entrada-padrão a carta puxada da pilha e guarde na sua mão.
-
-    Vale ressaltar que nada impede do bot comprar quantas cartas quiser, mesmo tendo uma carta
-    na mão com o valor ou naipe da mesa. Só não é possível comprar uma quantidade diferente de
-    cartas quando ele deve obrigatoriamente comprar 2 (por causa de um Valete) ou 4 (por causa
-    de um coringa).
-
-    Depois da carta lida, não há opção de descarte. Agora, é a vez de um outro bot jogar.
-
-    Além das ações de descartar (DISCARD) e comprar (BUY), o bot pode também enviar mensagens
-    para serem apresentadas no jogo. Essas mensagens não são repassadas para os outros bot, mas
-    aparecem no console. Para enviar uma mensagem, o bot deverá enviar para a saída-padrão o
-    seguinte comando:
-      SAY <text>
-
-    O bot pode enviar quantas mensagens quiser, desde que seja *ANTES* das ações de descarte ou
-    de compra. Alguns exemplos de mensagens são:
-      "SAY Caramba! Eu já ia bater!"
-      "SAY Tu tá lascado!!!"
-
-    Resumindo, o bot pode realizar uma das seguintes ações:
-    - "SAY <text>", onde <texto> é uma mensagem que irá aparecer durante a partida.
-    - "DISCARD <card> [naipe]", onde <card> é a carta da mão a ser descartada.
-      Se <card> for um Coringa (C) ou Ás (A), um naipe deve ser informado também.
-    - "BUY <num>", onde <num> é a quantidade de cartas a serem compradas da pilha.
-
-
-    OBS: Todas as mensagens enviadas **DEVEM terminar com salto de linha ('\n')**, caso
-         contrário, o simulador não saberá quando uma ação termina e quebrar o sincronização
-         das mensagens.
-
-    Qualquer ação enviada para o simulador que não seja condizente com o estado do jogo,
-    haverá uma penalidade para o bot.
-    - Se o bot descartar uma carta que não tem na mão ou se o naipe da carta não for o que
-      se encontra sobre a mesa, a ação será ignorada. Ou seja, para o simulador, o bot
-      continuará com a referida carta na mão.
-    - Se o bot precisar comprar 2 ou 4 cartas e não enviar a ação "BUY" com o complemento
-      correspondente, o bot sai do jogo e perde de vez a partida.
-
-    Outra penalidade é se o bot demorar mais de 3 segundos para responder uma ação. Isso
-    significa que a leitura e escrita dos dados está fora de sincronia com o simulador
-    (o bot esperando um dado do simulador e o simulador esperando um dado do bot).
-    Nesse caso, o bot também será removido da partida.
-    */
-
-    // Nesse exemplo de ação, o bot tenta descartar a carta 4♥.
-    // Se ele não tiver na mão, a ação é simplesmente ignorada.
-
-    // char card[] = "A♥ ♥";
-    // printf("DISCARD %s\n", card);
   }
 
   return 0;
